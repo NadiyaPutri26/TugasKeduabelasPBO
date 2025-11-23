36684064c302e22be5d802ff061177ae07b28857
Nadiya Putri Intan Nur Rahmadhani <nadiyarahmadhani481@gmail.com>
2025-11-23 22:05:11 +0700

    Upload tugas kedu belas pbo

---
+++ PertemuanKeempatbelas/build/generated-sources/ap-source-output/Login/Login_1_.java
@@ -0,0 +1,16 @@
+package Login;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-11-22T09:21:05", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Login_1.class)
+public class Login_1_ { 
+
+    public static volatile SingularAttribute<Login_1, String> password;
+    public static volatile SingularAttribute<Login_1, String> answer;
+    public static volatile SingularAttribute<Login_1, String> securityQuestion;
+    public static volatile SingularAttribute<Login_1, String> username;
+
+}
---
+++ PertemuanKeempatbelas/src/Login/Login_1.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Login;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Huawei
+ */
+@Entity
+@Table(name = "login")
+@NamedQueries({
+    @NamedQuery(name = "Login_1.findAll", query = "SELECT l FROM Login_1 l"),
+    @NamedQuery(name = "Login_1.findByUsername", query = "SELECT l FROM Login_1 l WHERE l.username = :username"),
+    @NamedQuery(name = "Login_1.findByPassword", query = "SELECT l FROM Login_1 l WHERE l.password = :password"),
+    @NamedQuery(name = "Login_1.findBySecurityQuestion", query = "SELECT l FROM Login_1 l WHERE l.securityQuestion = :securityQuestion"),
+    @NamedQuery(name = "Login_1.findByAnswer", query = "SELECT l FROM Login_1 l WHERE l.answer = :answer")})
+public class Login_1 implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "security_question")
+    private String securityQuestion;
+    @Column(name = "answer")
+    private String answer;
+
+    public Login_1() {
+    }
+
+    public Login_1(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSecurityQuestion() {
+        return securityQuestion;
+    }
+
+    public void setSecurityQuestion(String securityQuestion) {
+        this.securityQuestion = securityQuestion;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (username != null ? username.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Login_1)) {
+            return false;
+        }
+        Login_1 other = (Login_1) object;
+        if ((this.username == null && other.username != null) || (this.username != null && !this.username.equals(other.username))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Login.Login_1[ username=" + username + " ]";
+    }
+    
+}
